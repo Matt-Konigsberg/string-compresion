dba2c2018f5bc40cefeac36c8c8c68a096f0a32c
Matt Konigsberg <konigsbergm5@gmail.com>
2014-09-16 14:05:18 -0400

    Base master

--- reviewingTheBasics.java
+++ reviewingTheBasics.java
@@ -1,33 +1,16 @@
-
-/**
- * Write a description of class reviewingTheBasics here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class reviewingTheBasics
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class reviewingTheBasics
-     */
+public class reviewingTheBasics{
     public reviewingTheBasics()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    boolean b=false;
+    boolean b2=false;
+    
+    int x=5;
+    int y=5;
+    
+    String f = "f";
+    String f2= new String("f");
+    String f3 = f;
+    System.out.println(f== f2);
+    System.out.println(f3== f);
     }
 }
